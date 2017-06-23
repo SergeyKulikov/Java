fcce7d0897b8419d79bb7db07db5290e8b1c2d90
Sergey Kulikov <kulikov.sergey73@gmail.com>
2017-06-23 16:16:54 +0300

    Add files via upload
    Исследование скорости расчета по нескольким потокам

---
+++ Threads/J2L5.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Проверка скорости расчетов через один или несколько потоков.
+ *
+ * @author Sergey Kulikov
+ * @version 1.00 on 23.06.2017
+ */
+
+class J2L5 {
+	static final int THREADS = 50;				 	// Количество потоков (может больше кол-ва ядер)
+	static final int SIZE = 10000000;			 	// Размер массива для обработки
+	static final int H = SIZE / THREADS;		 	// Делим массив на количество потоков
+	static final int HR = SIZE-(H * THREADS);	 	// Возможно есть остаток от SIZE
+	
+	public static void main(String args[]) {
+	    float[] arr = new float[SIZE];		 				// Массив для одного потока
+	    float[] arrThreads = new float[SIZE];				// Массив для нескольких потоков
+	    ArrayList<MyThread> tr = new ArrayList<MyThread>();	// Будем хранить потоки здесь
+		
+		float[][] arrays = new float[THREADS][H]; 	// создаем массивы под кадый поток размерностью SIZE / THREADS
+		float[] arraysh = new float[HR]; 			// Массив, в который пишем хвостик массива, который получился от SIZE-(H * THREADS) 
+		
+		long finishTime = 0; // Фигня для расчета времени и результата
+		long passedTime = 0;
+		long time1, time2; 
+		long startTime = System.currentTimeMillis();
+		boolean rez = false;
+		
+		// one thread
+		fillArray(arr);	// Залили еднички в массив
+		MyThread v1 = new MyThread();
+		v1.arr = arr;
+		v1.start();
+		while (v1.isAlive()); // Ждем окончания потока, а то неправильно выведет время расчета
+		finishTime = System.currentTimeMillis();
+		time1 = finishTime-startTime;
+		System.out.println("Totally completed in " + (time1)+ " msec.\n");
+		
+		
+		// multy thread
+		fillArray(arrThreads); // Залили еднички в массив
+		startTime = System.currentTimeMillis();
+		for (int t=0; t<THREADS; t++) {
+			System.arraycopy(arrThreads, H*t, arrays[t], 0, H);
+			
+			tr.add(new MyThread());
+			tr.get(t).arr = arrays[t];
+			tr.get(t).startElement = H*t;
+			tr.get(t).passedTime = 0;
+			tr.get(t).start();
+		}
+		
+		for (int t=0; t<THREADS; t++) {
+			try {
+				tr.get(t).join();
+			} catch (InterruptedException ex) {
+				System.out.println(tr.get(t).getName()+" thread is interrupted.");
+			}
+		} // Ждем окончания всех созданных потоков, а то неправильно выведет время расчета
+
+		if (HR > 0) { // Уже после всех потоков обрабатываем хвост. мне так удобнее.
+			int t = THREADS;
+			System.arraycopy(arrThreads, H*t, arraysh, 0, HR);  // Размерность массива другая! (HR вместо H)
+			
+			tr.add(new MyThread());
+			tr.get(t).arr = arraysh;
+			tr.get(t).startElement = H*t;
+			tr.get(t).passedTime = 0;
+			tr.get(t).start();
+			try {
+				tr.get(t).join();
+			} catch (InterruptedException ex) {
+				System.out.println(tr.get(t).getName()+" thread is interrupted.");
+			}
+		} // Ждем окончания потока, а то неправильно выведет время расчета
+
+		for (int t=0; t<THREADS; t++) {
+			System.arraycopy(tr.get(t).arr, 0, arrThreads, H*t, H); // Собираем полный массив из кусочков
+		}
+		
+		if (HR > 0) { 
+			int t = THREADS;
+			System.arraycopy(tr.get(t).arr, 0, arrThreads, H*t, HR); // Дописываем хвост
+		}
+		
+		finishTime = System.currentTimeMillis();
+		
+		time2 = finishTime-startTime;
+		System.out.println("Totally completed in " + (time2)+ " msec for "+THREADS+" threads.");
+		System.out.printf("\nThreads work in %f times faster!", (float)((float)time1/(float)time2));
+		
+		rez = Arrays.equals(arr, arrThreads); // Сравнили результаты прямого расчета и по кускам.
+		System.out.printf("\nThe values of the arrays are%s the same.", rez ? "" : "nt\'t");
+    }
+	
+	public static void fillArray(float []arr) {
+		for (int i=0; i<arr.length; i++)
+			arr[i] = 1; 
+	}
+
+}
+
+
+class MyThread extends Thread {
+	public float[] arr;
+	long passedTime;
+	int startElement;
+		
+	public void countArray() {
+		for (int i=0; i<arr.length; i++) {
+			arr[i] = (float)(arr[i] * Math.sin(0.2f + (i+startElement) / 5) * Math.cos(0.2f + (i+startElement) / 5) * Math.cos(0.4f + (i+startElement) / 2)); 
+		}
+	}
+	
+	@Override
+	public void run() {
+        System.out.println(getName() + " has started.");
+		
+		long a = System.currentTimeMillis();
+		countArray();
+		passedTime = System.currentTimeMillis()-a;
+		System.out.println(getName() + " completed in " + (passedTime)+ " msec.");
+	}	
+}
